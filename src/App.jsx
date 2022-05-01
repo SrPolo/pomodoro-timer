4679ede9ed850acb0d26e3506e99780d644ca33b
Juan Pablo <juanpablopolancor22@gmail.com>
2022-05-01 12:27:24 -0500

    tailwind installation

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-2xl text-red-500">Pomodoro timer</h1>
+    </div>
+  )
+}
+
+export default App
--- src/PomodoroTimer.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>Pomodoro Timer</div>
-}
-
-export default App
