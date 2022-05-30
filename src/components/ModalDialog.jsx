6126b25a32f880107d88c708482fd9a3ca1c8dd5
Juan Pablo <juanpablopolancor22@gmail.com>
2022-05-29 22:09:44 -0500

    modal dialog

--- src/components/ModalDialog.jsx
+++ src/components/ModalDialog.jsx
@@ -1,12 +1,29 @@
-const ModalDialog = ({ children }) => {
-  return (
-    <div
-      className="fixed bg-gray-600 bg-opacity-50 overflow-y-auto top-0 left-0
-      py-12 w-full h-full flex justify-center items-center">
-      <div className="relative m-auto p-5 border w-96 shadow-lg rounded-md bg-white">
-        {children}
+import PropTypes from 'prop-types'
+
+const ModalDialog = (props) => {
+  const { children, isOpen, onClose } = props
+  if (isOpen) {
+    return (
+      <div
+        className="fixed bg-gray-600 bg-opacity-50 overflow-y-auto top-0 left-0
+          py-12 w-full h-full flex justify-center items-center"
+        onClick={onClose}>
+        <div
+          className="relative m-auto p-5 border w-96 shadow-lg rounded-md bg-white"
+          onClick={(e) => e.stopPropagation()}>
+          {children}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  return null
 }
+
 export default ModalDialog
+
+ModalDialog.propTypes = {
+  children: PropTypes.node.isRequired,
+  isOpen: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+}
