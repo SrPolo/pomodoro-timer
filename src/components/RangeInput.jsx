be2081b0eddb463c8ffc17f0829efe7d65149806
Juan Pablo <juanpablopolancor22@gmail.com>
2022-05-29 22:41:51 -0500

    RangeInput component

---
+++ src/components/RangeInput.jsx
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types'
+
+const RangeInput = (props) => {
+  const { value, onChange, name, max, min, label } = props
+  return (
+    <div className="w-full grid justify-items-center">
+      <label htmlFor={name}>{label}</label>
+      <span>{`${value} : 00`}</span>
+      <input
+        id={name}
+        name={name}
+        type="range"
+        max={max}
+        min={min}
+        value={value}
+        onChange={onChange}
+        className="w-2/3"
+      />
+    </div>
+  )
+}
+export default RangeInput
+
+RangeInput.propTypes = {
+  value: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  max: PropTypes.number,
+  min: PropTypes.number,
+  onChange: PropTypes.func,
+}
